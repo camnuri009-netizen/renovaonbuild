228e2ca7f6d3add6adc844a4001041b5dedf7117
인생노래쉼터 <camnuri009@gmail.com>
2026-07-16 09:53:34 +0900

    Update Firebase configuration with new credentials

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,8 @@
-// Firebase 콘솔 > 프로젝트 설정 > 내 앱 > 웹 앱에서 받은 값으로 교체하세요.
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyB5gd0Y2FQnNO1p3oVWG3F03IU164BzM6w",
+  authDomain: "renova-cms.firebaseapp.com",
+  projectId: "renova-cms",
+  storageBucket: "renova-cms.firebasestorage.app",
+  messagingSenderId: "610394764954",
+  appId: "1:610394764954:web:f4d000dd0d5c9b80a7d123"
 };
